936ab5d8cff61f0c810a18155da21adc52a5cebf
Priyapta <priaptanaufal@gmail.com>
2024-09-04 15:36:21 +0700

    "update"

--- main/views.py
+++ main/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 def show_main(request):
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'npm' : '2306245106',
+        'name': 'Priyapta Naufal',
+        'class': 'PBP D'
     }
 
     return render(request, "main.html", context)
